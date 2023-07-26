afcb08370a9e84ee6ddca63f325c140515da4000
강범수 <kbs21c1987@gmail.com>
2023-07-26 18:39:11 +0900

    파이어베이스 연동

--- udemy/routing/src/pages/AllMeetups.js
+++ udemy/routing/src/pages/AllMeetups.js
@@ -1,25 +1,28 @@
 import { useEffect, useState } from 'react';
 import MeetupList from '../components/Meetups/MeetupList';
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description: 'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description: 'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: 'm1',
+//     title: 'This is a first meetup',
+//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description: 'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+//   {
+//     id: 'm2',
+//     title: 'This is a second meetup',
+//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description: 'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+// ];
 
 const AllMeetups = () => {
   const [list, setList] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // console.log(list);
+  // console.log(Object.keys(list));
 
   useEffect(() => {
     console.log('fetch!!');
@@ -28,15 +31,28 @@ const AllMeetups = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const meetups = [];
+      const response = await fetch('');
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setList(data);
+      console.log(data);
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key],
+        };
+
+        meetups.push(meetup);
+      }
+
+      setList(meetups);
+      setLoading(false);
     } catch (error) {
       console.log('에러 내용', error);
       setError(error); // 에러 상태를 업데이트합니다.
+      setLoading(false);
     }
   };
 
--- udemy/routing/src/pages/NewMeetup.js
+++ udemy/routing/src/pages/NewMeetup.js
@@ -1,7 +1,19 @@
 import NewMeetupForm from '../components/Meetups/NewMeetupForm';
+import { useNavigate } from 'react-router-dom';
 
 const NewMeetup = () => {
-  return <NewMeetupForm />;
+  const history = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch('', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(() => {
+      history('/');
+    });
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 
 export default NewMeetup;
