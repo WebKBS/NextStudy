06e0c870c57be52662306bb1ad50efad21b0d87f
강범수 <kbs21c1987@gmail.com>
2023-07-26 16:28:54 +0900

    fetching

--- udemy/routing/src/components/Meetups/MeetupItem.js
+++ udemy/routing/src/components/Meetups/MeetupItem.js
@@ -1,19 +1,22 @@
+import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 const MeetupItem = ({ image, title, address, description }) => {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={image} alt={title} />
-      </div>
-      <div className={classes.content}>
-        <h3 className={classes.title}>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favoerites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={classes.content}>
+          <h3 className={classes.title}>{title}</h3>
+          <address>{address}</address>
+          <p>{description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favoerites</button>
+        </div>
+      </Card>
     </li>
   );
 };
--- udemy/routing/src/pages/AllMeetups.js
+++ udemy/routing/src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/Meetups/MeetupList';
 const DUMMY_DATA = [
   {
@@ -17,10 +18,33 @@ const DUMMY_DATA = [
 ];
 
 const AllMeetups = () => {
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log('fetch!!');
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setList(data);
+    } catch (error) {
+      console.log('에러 내용', error);
+      setError(error); // 에러 상태를 업데이트합니다.
+    }
+  };
+
   return (
     <section>
       <div>All Page</div>
-      <MeetupList meetups={DUMMY_DATA} />
+      {error && <div>에러 발생: {error.message}</div>}
+      <MeetupList meetups={list} />
     </section>
   );
 };
