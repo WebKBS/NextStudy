dcbb1d1837268863baf383ee7a3986e990d0643c
gangbeomsu <kbs21c1987@gmail.com>
2023-08-01 22:38:02 +0900

    동적 라우트 네비게이팅

--- udemy/next/page/pages/clients/index.tsx
+++ udemy/next/page/pages/clients/index.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "kang", name: "beom" },
+    { id: "manu", name: "manual" },
+  ];
+
   return (
     <div>
       <h1>Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
