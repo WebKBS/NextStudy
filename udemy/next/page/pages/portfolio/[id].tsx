61d6c9209c5bb5cf14b25e25f581652a5a5b49ae
gangbeomsu <kbs21c1987@gmail.com>
2023-08-01 22:00:37 +0900

    다이나믹 페이지

--- udemy/next/page/pages/portfolio/[id].tsx
+++ udemy/next/page/pages/portfolio/[id].tsx
@@ -1,6 +1,13 @@
 // 다이나믹 페이지
 
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query.id);
+
   return (
     <div>
       <h1>PortfolioProject Page</h1>
