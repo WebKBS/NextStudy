239eaf9a33815bf6d3cd8b25ed1137d997e88d0d
gangbeomsu <kbs21c1987@gmail.com>
2023-08-01 22:50:47 +0900

    프로그래밍방식 네비게이팅

--- udemy/next/page/pages/clients/[id]/index.tsx
+++ udemy/next/page/pages/clients/[id]/index.tsx
@@ -1,7 +1,22 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    // router.push("/clients/kang/projecta");
+    router.push({
+      pathname: "/clients/[id]/[clientproject]",
+      query: { id: "kang", clientproject: "Project" },
+    });
+  }
+
   return (
     <div>
       <h1>Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
