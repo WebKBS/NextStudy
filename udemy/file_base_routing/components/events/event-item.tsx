31441f42b1ff856ce55426fbb878c4aafb428eb5
강범수 <kbs21c1987@gmail.com>
2023-08-04 18:45:56 +0900

    아이콘 추가

--- udemy/file_base_routing/components/events/event-item.tsx
+++ udemy/file_base_routing/components/events/event-item.tsx
@@ -1,5 +1,10 @@
 // import Link from 'next/link';
 import Button from '../ui/button';
+
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+
 import classes from './event-item.module.css';
 
 interface Event {
@@ -31,15 +36,21 @@ function EventList(props: Event) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
-          {/* <Link href={exploreLink}>Explore Event</Link> */}
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
