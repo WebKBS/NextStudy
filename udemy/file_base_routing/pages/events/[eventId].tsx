af5a4a917ad673c72ac09d6a83ae2c310f60be22
gangbeomsu <kbs21c1987@gmail.com>
2023-08-05 15:52:18 +0900

    라우트별 데이터 입력하기

--- udemy/file_base_routing/pages/events/[eventId].tsx
+++ udemy/file_base_routing/pages/events/[eventId].tsx
@@ -1,8 +1,31 @@
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
 function EventDetailsPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Details Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+        title={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
   );
 }
 
