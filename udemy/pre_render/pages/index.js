93115d7c4a58d5edf39e3f19dda16c8cc26eb422
gangbeomsu <kbs21c1987@gmail.com>
2023-08-07 13:56:46 +0900

    getstaticprops 알아보기

--- udemy/pre_render/pages/index.js
+++ udemy/pre_render/pages/index.js
@@ -1,11 +1,24 @@
 function HomePage(props) {
+  const { products } = props;
+
+  console.log(products);
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// 항상 객체를 return 해야한다.
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "product 1" }],
+    },
+  };
 }
 
 export default HomePage;
