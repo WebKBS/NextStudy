51ae5316f6ee172d63a7505ecfea41b255a41ed2
gangbeomsu <kbs21c1987@gmail.com>
2023-08-07 14:41:28 +0900

    revalidate 설정

--- udemy/pre_render/pages/index.js
+++ udemy/pre_render/pages/index.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 function HomePage(props) {
   const { products } = props;
 
@@ -14,10 +17,16 @@ function HomePage(props) {
 
 // 항상 객체를 return 해야한다.
 export async function getStaticProps() {
+  console.log("re 제네레이트");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.promises.readFile(filePath); // await를 사용하여 비동기적으로 호출
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "product 1" }],
+      products: data.products,
     },
+    revalidate: 10, // 시간을 부여해서 시간에 따라 변경사항을 추가한다
   };
 }
 
