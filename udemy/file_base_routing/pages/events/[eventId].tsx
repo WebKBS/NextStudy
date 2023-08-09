c1e5de3e6f7c06e7cf311ee6cc3b463768cbab80
gangbeomsu <kbs21c1987@gmail.com>
2023-08-09 22:01:57 +0900

    다이나믹 페이지에 데이터 가져오기

--- udemy/file_base_routing/pages/events/[eventId].tsx
+++ udemy/file_base_routing/pages/events/[eventId].tsx
@@ -1,15 +1,32 @@
-import { getEventById } from "@/dummy-data";
+import { getEventById, getAllEvents } from "@/dummy-data";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventContent from "@/components/event-detail/event-content";
 import ErrorAlert from "@/components/events/error-alert";
+import { GetStaticPropsContext } from "next";
 
-function EventDetailsPage() {
+interface EventType {
+  id: string; // 이벤트 고유 ID
+  title: string;
+  date: string;
+  location: string;
+  image: string;
+  description: string;
+}
+
+interface PropsType {
+  selectEvent: EventType; // 단일 이벤트 객체
+}
+
+function EventDetailsPage(props: PropsType) {
   const router = useRouter();
   const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+  // const event = getEventById(eventId);
+  const event = props.selectEvent;
+
+  console.log(event);
 
   if (!event) {
     return (
@@ -32,6 +49,28 @@ function EventDetailsPage() {
       <EventContent>{event.description}</EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const eventId = context.params?.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const path = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 }
 
 export default EventDetailsPage;
