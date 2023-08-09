b9f034e5240081a30680a295ddf7f8577b0dd64e
gangbeomsu <kbs21c1987@gmail.com>
2023-08-09 18:20:28 +0900

    swr사용해서 데이터 가져오기

--- udemy/pre_render/pages/last-sales.js
+++ udemy/pre_render/pages/last-sales.js
@@ -1,41 +1,67 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-const url = "";
+const url = "https://test-5ae71-default-rtdb.firebaseio.com/sales.json";
 
 function LastSalesPage() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  console.log(data);
-  useEffect(() => {
-    setIsLoading(true);
-    console.log("useEffect");
-    fetch(url)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then((data) => {
-        const transformedData = [];
-        for (const key in data) {
-          transformedData.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setData(transformedData);
-        setIsLoading(false);
-      });
-  }, []);
+  const [sales, setSales] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  if (isLoading) {
+  const { data, error } = useSWR(url, fetcher);
+  console.log(data);
+  async function fetcher(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    if (data) {
+      const transformedData = [];
+      for (const key in data) {
+        transformedData.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedData);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(url)
+  //     .then((res) => {
+  //       if (res.ok) {
+  //         return res.json();
+  //       }
+  //     })
+  //     .then((data) => {
+  //       const transformedData = [];
+  //       for (const key in data) {
+  //         transformedData.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setData(transformedData);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (!data || !sales) {
     return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Failed to load</p>;
   }
 
   return (
     <ul>
-      {data.map((sale) => (
+      {sales.map((sale) => (
         <li key={sale.id}>
           {sale.username} - {sale.volume}
         </li>
