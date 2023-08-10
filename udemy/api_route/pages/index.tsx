e9736b0043bb2347b53f35448d401bd61641c836
gangbeomsu <kbs21c1987@gmail.com>
2023-08-10 19:51:32 +0900

    폼 만들기

--- udemy/api_route/pages/index.tsx
+++ udemy/api_route/pages/index.tsx
@@ -1,7 +1,39 @@
+import React, { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const feedbackInputRef = useRef<HTMLTextAreaElement>(null);
+
+  function submitFormHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (emailInputRef.current && feedbackInputRef.current) {
+      const enteredEmail = emailInputRef.current.value;
+      const enteredFeedback = feedbackInputRef.current.value;
+
+      console.log(enteredEmail, enteredFeedback);
+    }
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form action="">
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">당신의 피드백</label>
+          <textarea
+            name=""
+            id="feedback"
+            rows={10}
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
