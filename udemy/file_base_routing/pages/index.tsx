fd5465a364307016b63f035d7274605a4c5f3a34
gangbeomsu <kbs21c1987@gmail.com>
2023-08-10 17:44:42 +0900

    head 추가하기

--- udemy/file_base_routing/pages/index.tsx
+++ udemy/file_base_routing/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
@@ -8,6 +9,12 @@ interface propsType {
 function HomePage(props: propsType) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find the latest events" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
