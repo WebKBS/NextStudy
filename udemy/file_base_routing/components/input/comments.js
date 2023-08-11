9b26af33e4493182c673f5598c430405f3438948
gangbeomsu <kbs21c1987@gmail.com>
2023-08-12 00:25:03 +0900

    코멘트 메세지 상태 처리 및 로딩 추가

--- udemy/file_base_routing/components/input/comments.js
+++ udemy/file_base_routing/components/input/comments.js
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import classes from "./comments.module.css";
+import NotificationContext from "@/store/notification-context";
 
 function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsLoading(true);
       fetch(`/api/comments/${eventId}`)
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
           setComments(data.comments);
+          setIsLoading(false);
         });
     }
   }, [eventId, showComments]);
@@ -26,6 +32,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: "전송중..",
+      message: "기다리세요...",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -33,8 +45,28 @@ function Comments(props) {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "성공",
+          message: "코멘트 입력 완료",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!!",
+          message: error.message,
+          status: "error",
+        });
+      });
   }
 
   return (
@@ -43,7 +75,8 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isLoading && <CommentList items={comments} />}
+      {showComments && isLoading && <p>Loading...</p>}
     </section>
   );
 }
