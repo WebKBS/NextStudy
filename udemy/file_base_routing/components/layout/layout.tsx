3715c386d54ef315e5306ed549098060c03fc6a5
gangbeomsu <kbs21c1987@gmail.com>
2023-08-12 00:03:00 +0900

    useContext로 에러 상태 만들기

--- udemy/file_base_routing/components/layout/layout.tsx
+++ udemy/file_base_routing/components/layout/layout.tsx
@@ -1,16 +1,39 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+import NotificationContext from "@/store/notification-context";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
+interface NotificationContextType {
+  notification: {
+    title: string;
+    message: string;
+    status: string;
+  } | null;
+}
 function Layout({ children }: LayoutProps) {
+  const notificationCtx = useContext(
+    NotificationContext
+  ) as NotificationContextType;
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
+
 export default Layout;
