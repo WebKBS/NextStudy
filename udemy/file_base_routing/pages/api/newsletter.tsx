570c29a81ef290228e80b8f620f90e5e71ea700b
gangbeomsu <kbs21c1987@gmail.com>
2023-08-11 22:36:06 +0900

    에러 처리 추가

--- udemy/file_base_routing/pages/api/newsletter.tsx
+++ udemy/file_base_routing/pages/api/newsletter.tsx
@@ -2,6 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { MongoClient } from "mongodb";
 const url = process.env.MONGO_URI;
 
+async function connectToDatabase() {
+  const client = await MongoClient.connect(url!);
+  return client;
+}
+
+async function insertDocument(client: any, document: object) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -11,12 +21,21 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       return;
     }
 
-    const client = await MongoClient.connect(url!);
-    const db = client.db();
+    let client;
+    try {
+      client = await connectToDatabase();
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "데이터베이스 연결실패" });
+    }
 
-    await db.collection("emails").insertOne({ email: userEmail });
-
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client!.close();
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "데이터베이스 추가 실패" });
+    }
 
     res.status(201).json({ message: "Success" });
   }
