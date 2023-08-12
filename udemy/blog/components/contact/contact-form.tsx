811815ac225bcfab4645e3b9e0b20fbb34081ac5
gangbeomsu <kbs21c1987@gmail.com>
2023-08-12 23:14:52 +0900

    에러메세지 구현

--- udemy/blog/components/contact/contact-form.tsx
+++ udemy/blog/components/contact/contact-form.tsx
@@ -1,13 +1,34 @@
 import { FormEvent, useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+
+async function sendContactData(contactDetails: object) {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "메세지 전송 실패");
+  }
+}
 
 function ContactForm() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredName, setEnteredName] = useState("");
   const [enteredMessage, setEntereMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState<string>(); // pending, success, error
+  const [requestError, setRequestError] = useState<string>();
 
-  function sendMessageHandler(event: FormEvent) {
+  async function sendMessageHandler(event: FormEvent) {
     event.preventDefault();
+
+    setRequestStatus("pending");
 
     const postData = {
       email: enteredEmail,
@@ -15,24 +36,39 @@ function ContactForm() {
       message: enteredMessage,
     };
 
-    fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          alert("에러발생");
-          console.log(response);
-          return;
-        }
-        console.log(response);
-      })
-      .catch((err) => {
-        console.error("Error sending message:", err);
-      });
+    try {
+      await sendContactData(postData);
+      setRequestStatus("success");
+    } catch (err) {
+      setRequestError(err instanceof Error ? err.message : "메세지 전송 실패");
+      setRequestStatus("error");
+    }
+  }
+
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "메세지를 전송중입니다.",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: requestError,
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: "메세지 전송 실패",
+    };
   }
 
   return (
@@ -72,6 +108,13 @@ function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }
