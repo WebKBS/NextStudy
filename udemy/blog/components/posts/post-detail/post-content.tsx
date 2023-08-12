5ab80609853ac5bf64ae8064d0aac95a3531f397
gangbeomsu <kbs21c1987@gmail.com>
2023-08-12 20:49:34 +0900

    코드 마크다운 추가하기

--- udemy/blog/components/posts/post-detail/post-content.tsx
+++ udemy/blog/components/posts/post-detail/post-content.tsx
@@ -1,16 +1,58 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
+import Image from "next/image";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
-import { Post } from "@/types/post";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 function PostContent({ post }: any) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // img(image: any) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt="test-image"
+    //     />
+    //   );
+    // },
+    p(paragraph: any) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt="test-image"
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code: any) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
