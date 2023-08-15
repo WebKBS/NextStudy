9d5c77c8c8570a9ede9108cb2eac1dc5affb4e75
gangbeomsu <kbs21c1987@gmail.com>
2023-08-15 21:04:45 +0900

    프론트에서 라우트 보호하기

--- udemy/next-auth/pages/auth.js
+++ udemy/next-auth/pages/auth.js
@@ -1,6 +1,26 @@
 import AuthForm from "../components/auth/auth-form";
+import { useEffect, useState } from "react";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.push("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return <AuthForm />;
 }
 
