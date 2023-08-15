fab7de1273a58e367f4a95e235617987ac2bf96c
gangbeomsu <kbs21c1987@gmail.com>
2023-08-15 19:24:40 +0900

    next auth 설정하기

---
+++ udemy/next-auth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,43 @@
+import { verifyPassword } from "@/lib/auth";
+import { connetToDatabase } from "@/lib/db";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+// https://next-auth.js.org/providers/credentials 참고
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const client = await connetToDatabase();
+        const usersCollection = client.db().collection("users");
+
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          throw new Error("등록된 유저가 없습니다.");
+        }
+
+        // compare 된 비밀번호 일치 확인
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          throw new Error("비밀번호가 일치하지 않습니다.");
+        }
+
+        client.close();
+
+        // 비밀번호는 절대 내보내지 않는다.
+        return { email: user.email };
+      },
+    }),
+  ],
+});
