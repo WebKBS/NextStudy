a94f03165e12b283d7ad1d44364a899cd97430f1
강범수 <kbs21c1987@gmail.com>
2023-09-05 11:23:27 +0900

    방문자 카운터 구현

--- cookie/src/app/page.tsx
+++ cookie/src/app/page.tsx
@@ -1,10 +1,7 @@
-import UserCount from '@/components/userCount/userCount';
-
 export default async function Home() {
   return (
     <main>
       <h1>Hello</h1>
-      <UserCount />
     </main>
   );
 }
--- cookie/src/components/userCount/userCount.tsx
+++ cookie/src/components/userCount/userCount.tsx
@@ -1,31 +1,54 @@
 'use client';
+
 import { useState, useEffect } from 'react';
 
 export default function UserCount() {
   const [visitorCount, setVisitorCount] = useState(0);
 
   useEffect(() => {
-    // 페이지가 로드될 때마다 localStorage에서 저장된 카운트를 가져옵니다.
-    const storedCount = localStorage.getItem('visitorCount');
-    if (storedCount) {
-      setVisitorCount(parseInt(storedCount));
-    }
+    // 로컬 스토리지에서 저장된 방문자 수 가져오기
+    const storedCount = parseInt(localStorage.getItem('visitorCount') || '0', 10);
+
+    // 초기 방문자 수 설정
+    setVisitorCount(storedCount);
+
+    // 로컬 스토리지 값이 변경될 때마다 화면에 반영
+    const updateVisitorCount = (event: StorageEvent) => {
+      // key를 통해 카운트 매치
+      if (event.key === 'visitorCount') {
+        const updatedCount = parseInt(event.newValue || '0', 10);
+        setVisitorCount(updatedCount);
+      }
+    };
+
+    // 이벤트 리스너 등록
+    window.addEventListener('storage', updateVisitorCount);
+
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      window.removeEventListener('storage', updateVisitorCount);
+    };
   }, []);
 
   useEffect(() => {
-    // visitorCount가 변경될 때마다 localStorage에 새로운 값으로 저장합니다.
-    localStorage.setItem('visitorCount', visitorCount.toString());
-  }, [visitorCount]);
+    // 페이지가 로드될 때마다 방문자 수 증가 및 로컬 스토리지에 저장
+    const increaseVisitorCount = () => {
+      setVisitorCount((prevCount) => {
+        const newCount = prevCount + 1;
+        localStorage.setItem('visitorCount', newCount.toString());
+        return newCount;
+      });
+    };
 
-  const incrementCount = () => {
-    setVisitorCount((prevCount) => prevCount + 1);
-  };
+    increaseVisitorCount();
+
+    // 컴포넌트가 마운트되었을 때 한 번만 실행되도록 빈 배열 전달
+  }, []);
 
   return (
     <div>
       <h1>Welcome to My Blog</h1>
-      <p>Visitor Count: {visitorCount}</p>
-      <button onClick={incrementCount}>Increment Count</button>
+      <p>오늘의 방문자: {visitorCount}</p>
     </div>
   );
 }
