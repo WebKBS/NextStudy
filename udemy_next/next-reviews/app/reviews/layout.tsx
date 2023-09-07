d29b66d3da07fc86c6a3fab343e5ef9a7305e0ee
gangbeomsu <kbs21c1987@gmail.com>
2023-09-07 22:12:59 +0900

    각 페이지와 개별layout 사용

---
+++ udemy_next/next-reviews/app/reviews/layout.tsx
@@ -0,0 +1,8 @@
+export default function ReviewsLayout({ children }) {
+  return (
+    <div style={{ display: "flex" }}>
+      <h3 style={{ border: "2px solid #ff0" }}>Reviews NavBar</h3>
+      {children}
+    </div>
+  );
+}
---
+++ udemy_next/next-reviews/app/reviews/page.tsx
@@ -0,0 +1,8 @@
+export default function ReviewsPage() {
+  return (
+    <div>
+      <h1>Reviews</h1>
+      <p>리뷰 페이지</p>
+    </div>
+  );
+}
