f4022e2d8806351e3d30b1e632ebe688a8fb57af
gangbeomsu <kbs21c1987@gmail.com>
2023-09-09 20:14:19 +0900

    테일윈드 css 사용해보기

--- udemy_next/next-reviews/app/layout.tsx
+++ udemy_next/next-reviews/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Link from "next/link";
-
+import './globals.css';
+import NavBar from "../components/NavBar";
 interface LayoutProps {
   children: ReactNode;
 }
@@ -8,26 +9,14 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col px-4 py-2 min-h-screen bg-orange-50">
         <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/reviews" prefetch={false}>Reviews</Link>
-                    </li>
-                    <li>
-                        <Link href="/about">About</Link>
-                    </li>
-                </ul>
-            </nav>
+       <NavBar/>
         </header>
-        <main>{children}</main>
-        <footer>
-            네이버 연결하기
-            <a href="https://naver.com" target="_blank">네이버</a>
+        <main className="py-3 grow">{children}</main>
+        <footer className="border-t py-3 text-center text-xs">
+            네이버 연결하기{" "}
+            <a href="https://naver.com" className="hover:underline" target="_blank">네이버</a>
         </footer>
       </body>
     </html>
