357544f375ea96498819251ec76d22c8afe2a7dd
gangbeomsu <kbs21c1987@gmail.com>
2023-09-10 19:10:03 +0900

    md 파일 불러오기 재사용하기

--- udemy_next/next-reviews/app/reviews/layout.tsx
+++ udemy_next/next-reviews/app/reviews/layout.tsx
@@ -1,7 +1,7 @@
 export default function ReviewsLayout({ children }) {
   return (
     <div>
-      <h3>Reviews NavBar</h3>
+      {/*<h3>Reviews NavBar</h3>*/}
       {children}
     </div>
   );
--- udemy_next/next-reviews/app/reviews/start/page.tsx
+++ udemy_next/next-reviews/app/reviews/start/page.tsx
@@ -1,24 +1,21 @@
-import {readFile} from "node:fs/promises";
-import {marked} from "marked";
-import matter from "gray-matter"
+// import getReview from "@/lib/reviews"
+import {getReview} from "@/lib/reviews";
 import Heading from "@/components/Heading";
 
-export default async function StartPage() {
-    const text = await readFile("./content/reviews/stardew-valley.md", "utf-8");
-    const {content, data: {title, date, image}} = matter(text);
-    const html = marked(content);
 
+export default async function StartPage() {
+    const review = await getReview("stardew-valley")
     // console.log(data.title)
 
     return (
         <>
-            <Heading>{title}</Heading>
-            <p className="italic pb-2">{date}</p>
-            <img src={image} alt="" width="640"
+            <Heading>{review.title}</Heading>
+            <p className="italic pb-2">{review.date}</p>
+            <img src={review.image} alt="" width="640"
                  height="360"
                  className="mb-2 rounded"/>
             {/* 외부로부터 html 태그를 입력할때는 dangerouslySetInnerHTML 를 사용해야한다. */}
-            <article dangerouslySetInnerHTML={{__html: html}}
+            <article dangerouslySetInnerHTML={{__html: review.body}}
                      className="max-w-screen-sm prose prose-slate"/>
         </>
     );
