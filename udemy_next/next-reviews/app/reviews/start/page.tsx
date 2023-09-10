d60b02964abb38c115f014fb39aa0adc4c687319
gangbeomsu <kbs21c1987@gmail.com>
2023-09-10 18:10:18 +0900

    마크다운 파일 읽고 html 입력해보기

--- udemy_next/next-reviews/app/reviews/start/page.tsx
+++ udemy_next/next-reviews/app/reviews/start/page.tsx
@@ -1,10 +1,18 @@
+import {readFile} from "node:fs/promises";
+import {marked} from "marked";
 import Heading from "@/components/Heading";
 
-export default function StartPage() {
-  return (
-    <>
-        <Heading>Start</Heading>
-      <p>스타트 페이지</p>
-    </>
-  );
+export default async function StartPage() {
+    const text = await readFile("./content/reviews/stardew-valley.md", "utf-8")
+    const html = marked(text)
+    return (
+        <>
+            <Heading>Start</Heading>
+            <img src="/images/stardew-valley.jpg" alt="" width="640"
+                 height="360"
+                 className="mb-2 rounded"/>
+            {/* 외부로부터 html 태그를 입력할때는 dangerouslySetInnerHTML 를 사용해야한다. */}
+            <article dangerouslySetInnerHTML={{__html: html}}/>
+        </>
+    );
 }
---
+++ udemy_next/next-reviews/content/reviews/stardew-valley.md
@@ -0,0 +1,13 @@
+# Heading
+
+## Sub-heading
+
+Rich text with __bold__ and _italic_.
+
+Another paragraph.
+
+List:
+
+* one
+* two
+* three
