016529ce0cfd122b12d0d349a739a91fa7a6619d
gangbeomsu <kbs21c1987@gmail.com>
2023-09-10 17:43:00 +0900

    테일윈드 반응형 사용해보기

--- udemy_next/next-reviews/app/page.tsx
+++ udemy_next/next-reviews/app/page.tsx
@@ -1,10 +1,23 @@
 import Heading from "@/components/Heading";
+import Link from "next/link";
 
 export default function HomePage() {
-  return (
-    <>
-    <Heading>Indie Game</Heading>
-      <p>Welcome</p>
-    </>
-  );
+    return (
+        <>
+            <Heading>Indie Game</Heading>
+            <p className="pb-3">Welcome</p>
+            <div
+                className="border w-80 bg-white rounded shadow hover:shadow-xl sm:w-full">
+                <Link href='/reviews/start'
+                      className="flex flex-col sm:flex-row">
+                    <img src="/images/stardew-valley.jpg" alt="" width="320"
+                         height="180"
+                         className="rounded-t sm:rounded-l sm:rounded-r-none"/>
+                    <h2 className="font-semibold py-1 text-center font-orbitron sm:px-2">
+                        Start
+                    </h2>
+                </Link>
+            </div>
+        </>
+    );
 }
