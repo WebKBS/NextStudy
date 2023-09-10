4ab452e47ac1f9fb3204a9bbab1ff7b25c298684
gangbeomsu <kbs21c1987@gmail.com>
2023-09-10 14:22:58 +0900

    카드만들기

--- udemy_next/next-reviews/app/reviews/layout.tsx
+++ udemy_next/next-reviews/app/reviews/layout.tsx
@@ -1,7 +1,7 @@
 export default function ReviewsLayout({ children }) {
   return (
-    <div style={{ display: "flex" }}>
-      <h3 style={{ border: "2px solid #ff0" }}>Reviews NavBar</h3>
+    <div>
+      <h3>Reviews NavBar</h3>
       {children}
     </div>
   );
--- udemy_next/next-reviews/app/reviews/page.tsx
+++ udemy_next/next-reviews/app/reviews/page.tsx
@@ -2,20 +2,28 @@ import Link from "next/link";
 import Heading from "@/components/Heading";
 
 export default function ReviewsPage() {
-  return (
-    <div>
-        <Heading>Reviews</Heading>
-      <p>리뷰 페이지</p>
-        <ul>
-            <li>
-                <Link href='/reviews/hollow-knight'>
-                    Hollow Knight
-                </Link>
-            </li>
-            <li>
-                <Link href='/reviews/start'>Start</Link>
-            </li>
-        </ul>
-    </div>
-  );
+    return (
+        <div>
+            <Heading>Reviews</Heading>
+            <p>리뷰 페이지</p>
+            <ul className="flex flex-col gap-3">
+                <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
+                    <Link href='/reviews/hollow-knight'>
+                        <img src="/images/hollow-knight.jpg" alt="" width="320" height="180" className="rounded-t"/>
+                        <h2 className="py-1 text-center">
+                            Hollow Knight
+                        </h2>
+                    </Link>
+                </li>
+                <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
+                    <Link href='/reviews/start'>
+                        <img src="/images/stardew-valley.jpg" alt="" width="320" height="180" className="rounded-t"/>
+                        <h2 className="py-1 text-center">
+                            Start
+                        </h2>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
