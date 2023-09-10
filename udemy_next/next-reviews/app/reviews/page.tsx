ba23a0620dc330d176713b6edaa559384b11de76
gangbeomsu <kbs21c1987@gmail.com>
2023-09-10 20:52:28 +0900

    다이나믹 링크 만들기 파일 필터하는방법

--- udemy_next/next-reviews/app/reviews/page.tsx
+++ udemy_next/next-reviews/app/reviews/page.tsx
@@ -1,28 +1,30 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import {getReviews} from "@/lib/reviews";
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+    const reviews = await getReviews();
+
+    console.log(reviews)
     return (
         <div>
             <Heading>Reviews</Heading>
             <p>리뷰 페이지</p>
             <ul className="flex flex-col gap-3">
-                <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
-                    <Link href='/reviews/hollow-knight'>
-                        <img src="/images/hollow-knight.jpg" alt="" width="320" height="180" className="rounded-t"/>
-                        <h2 className="font-semibold py-1 text-center font-orbitron">
-                            Hollow Knight
-                        </h2>
-                    </Link>
-                </li>
-                <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
-                    <Link href='/reviews/start'>
-                        <img src="/images/stardew-valley.jpg" alt="" width="320" height="180" className="rounded-t"/>
-                        <h2 className="font-semibold py-1 text-center font-orbitron">
-                            Start
-                        </h2>
-                    </Link>
-                </li>
+                {reviews.map((file) => (
+
+                    <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
+                        <Link href={`/reviews/${file.slug}`}>
+                            <img src={`${file.image}`}
+                                 alt={`${file.title}`}
+                                 width="320"
+                                 height="180" className="rounded-t"/>
+                            <h2 className="font-semibold py-1 text-center font-orbitron">
+                                {file.title}
+                            </h2>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
--- udemy_next/next-reviews/lib/reviews.ts
+++ udemy_next/next-reviews/lib/reviews.ts
@@ -1,13 +1,31 @@
-import {readFile} from "node:fs/promises";
+import {readdir, readFile} from "node:fs/promises";
 import matter from "gray-matter";
 import {marked} from "marked";
 
 export async function getReview(slug: string) {
-    const text = await readFile(`./content/reviews/${slug}.md`, "utf-8");
+    const text = await readFile(`./content/reviews/${slug}.md`, "utf-8")
+
     const {content, data: {title, date, image}} = matter(text);
     const body = marked(content);
 
     return {
-        title, date, image, body
+        // slug도 반드시 전달한다. url 링크에 사용
+        slug, title, date, image, body
     }
+}
+
+export async function getReviews() {
+    const files = await readdir("./content/reviews");
+
+    // file중에 md파일만 필터링한다. 중요~!!
+    const slugs = files.filter((file) => file.endsWith(".md")).map(file =>
+        file.slice(0, -".md".length));
+
+    const reviews = [];
+    for (const slug of slugs) {
+        const review = await getReview(slug);
+        reviews.push(review);
+    }
+
+    return reviews;
 }
