54a6a36ea7396d51bf34e99737d518c7f7691cc3
강범수 <kbs21c1987@gmail.com>
2023-09-13 14:23:35 +0900

    다이나믹 라우트를 정적으로(SSG) 생성하기

--- _udemy_next/next-reviews/app/page.tsx
+++ _udemy_next/next-reviews/app/page.tsx
@@ -1,20 +1,21 @@
 import Heading from "@/components/Heading";
 import Link from "next/link";
+import {getFeaturedReview} from "@/lib/reviews";
 
-export default function HomePage() {
+export default async function HomePage() {
+    const review = await getFeaturedReview();
     return (
         <>
             <Heading>Indie Game</Heading>
             <p className="pb-3">Welcome</p>
             <div
                 className="border w-80 bg-white rounded shadow hover:shadow-xl sm:w-full">
-                <Link href='/reviews/stardew-valley'
-                      className="flex flex-col sm:flex-row">
-                    <img src="/images/stardew-valley.jpg" alt="" width="320"
+                <Link href={`/reviews/${review.slug}`} className="flex flex-col sm:flex-row">
+                    <img src={review.image} alt="" width="320"
                          height="180"
                          className="rounded-t sm:rounded-l sm:rounded-r-none"/>
                     <h2 className="font-semibold py-1 text-center font-orbitron sm:px-2">
-                        Start
+                        {review.title}
                     </h2>
                 </Link>
             </div>
--- _udemy_next/next-reviews/lib/reviews.ts
+++ _udemy_next/next-reviews/lib/reviews.ts
@@ -2,6 +2,12 @@ import {readdir, readFile} from "node:fs/promises";
 import matter from "gray-matter";
 import {marked} from "marked";
 
+
+export async  function  getFeaturedReview(){
+    const reviews = await  getReviews();
+    return reviews[0];
+}
+
 export async function getReview(slug: string) {
     const text = await readFile(`./content/reviews/${slug}.md`, "utf-8")
 
@@ -19,13 +25,21 @@ export async function getReviews() {
 
     const slugs = await  getSlugs();
 
-    console.log(slugs)
+    // console.log(slugs)
 
     const reviews = [];
     for (const slug of slugs) {
         const review = await getReview(slug);
         reviews.push(review);
     }
+
+    
+    // date기준으로 sorting 하는 방법
+    reviews.sort((a, b) => {
+        return  a.date.localeCompare(b.date);
+        // return  b.date.localeCompare(a.date);
+    })
+
 
     return reviews;
 }
