f3ed2aba976ac4aa95b3730a185f252d27160f25
강범수 <kbs21c1987@gmail.com>
2023-09-14 11:12:30 +0900

    클립보드 추가

--- _udemy_next/next-reviews/components/ShareLinkButton.tsx
+++ _udemy_next/next-reviews/components/ShareLinkButton.tsx
@@ -1,14 +1,24 @@
 'use client';
 
+import {useState} from "react";
+
 export default function ShareLinkButton() {
+    const [clicked, setClicked] = useState(false);
+
+
     const handleClick = () => {
-        console.log("Hello")
+        navigator.clipboard.writeText(window.location.href).then(_ => {
+            setClicked(true);
+            setTimeout(() => setClicked(false), 1500);
+        });
+        console.log("Hello", clicked);
+
     }
     return (
         <button
             onClick={handleClick}
             className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700">
-            Share Link
+            {clicked ? "Link Copied" : "Share link"}
         </button>
 
     )
