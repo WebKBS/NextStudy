479045b69f55895c02a86d2e1e20aa824c187151
gangbeomsu <kbs21c1987@gmail.com>
2023-09-25 20:33:31 +0900

    Image 컴포넌트 이미지 지연과 빠른 로딩 추가하기

--- _udemy_next/next-reviews/app/reviews/[slug]/page.tsx
+++ _udemy_next/next-reviews/app/reviews/[slug]/page.tsx
@@ -1,46 +1,52 @@
 // import getReview from "@/lib/reviews"
-import {getReview, getSlugs} from "@/lib/reviews";
+import { getReview, getSlugs } from "@/lib/reviews";
 import Heading from "@/components/Heading";
 import ShareButtons from "@/components/ShareButtons";
-
+import Image from "next/image";
 
 // 정적 사이트 SSG 생성
 export async function generateStaticParams() {
-    const slugs = await getSlugs();
+  const slugs = await getSlugs();
 
-    console.log(slugs)
+  console.log(slugs);
 
-    return slugs.map(slug => ({slug}));
+  return slugs.map((slug) => ({ slug }));
 }
-
 
 // 다이나믹 라우트 메타데이터 설정
-export async function generateMetadata({params: {slug}}): Promise<any> {
-    const review = await getReview(slug);
+export async function generateMetadata({ params: { slug } }): Promise<any> {
+  const review = await getReview(slug);
 
-    return {
-        // return으로 슬러그를 추출해서 타이틀을 내보낸다.
-        title: review.title
-    }
+  return {
+    // return으로 슬러그를 추출해서 타이틀을 내보낸다.
+    title: review.title,
+  };
 }
 
-export default async function ReviewPage({params: {slug}}) {
-    const review = await getReview(slug)
-    // console.log(review)
+export default async function ReviewPage({ params: { slug } }) {
+  const review = await getReview(slug);
+  // console.log(review)
 
-    return (
-        <>
-            <Heading>{review.title}</Heading>
-            <div className="flex gap-3 items-baseline">
-                <p className="italic pb-2">{review.date}</p>
-                <ShareButtons/>
-            </div>
-            <img src={review.image} alt="" width="640"
-                 height="360"
-                 className="mb-2 rounded"/>
-            {/* 외부로부터 html 태그를 입력할때는 dangerouslySetInnerHTML 를 사용해야한다. */}
-            <article dangerouslySetInnerHTML={{__html: review.body}}
-                     className="max-w-screen-sm prose prose-slate"/>
-        </>
-    );
+  return (
+    <>
+      <Heading>{review.title}</Heading>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareButtons />
+      </div>
+      <Image
+        src={review.image}
+        alt=""
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+        priority
+      />
+      {/* 외부로부터 html 태그를 입력할때는 dangerouslySetInnerHTML 를 사용해야한다. */}
+      <article
+        dangerouslySetInnerHTML={{ __html: review.body }}
+        className="max-w-screen-sm prose prose-slate"
+      />
+    </>
+  );
 }
