c923464f4aaca15ad9300c25c5d84d343ee89219
gangbeomsu <kbs21c1987@gmail.com>
2023-09-25 20:23:43 +0900

    Image 컴포넌트 이미지 요청 주소 설정

--- _udemy_next/next-reviews/next.config.js
+++ _udemy_next/next-reviews/next.config.js
@@ -4,8 +4,15 @@
  * @type {import('next').NextConfig}
  * */
 module.exports = {
-    output: 'export',
+    // output: 'export',
     images: {
-        "unoptimized": true
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "1337",
+                pathname: "/uploads/**"
+            }
+        ]
     }
 }
