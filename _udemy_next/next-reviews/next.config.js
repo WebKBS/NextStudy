397c614824e9bdbf8f84925dbe69bd3c27b318d9
강범수 <kbs21c1987@gmail.com>
2023-10-05 14:46:58 +0900

    added

--- _udemy_next/next-reviews/next.config.js
+++ _udemy_next/next-reviews/next.config.js
@@ -4,5 +4,13 @@
  * @type {import('next').NextConfig}
  * */
 module.exports = {
-    output: 'export'
+    // output: 'export'
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost"
+            }
+        ]
+    }
 }
