7ac289c4fb7c0ead47aab497ec907bd7d43e1256
gangbeomsu <kbs21c1987@gmail.com>
2023-10-08 20:35:17 +0900

    페이지네이션 페이지 조건 제어 설정하기

--- _udemy_next/next-reviews/components/PaginationBar.tsx
+++ _udemy_next/next-reviews/components/PaginationBar.tsx
@@ -4,20 +4,32 @@ import Link from "next/link";
 export default function PaginationBar({ href, page, pageCount }) {
   return (
     <div className="flex gap-2 items-center pb-3">
-      <PaginationLink href={`${href}?page=${page - 1}`}>
+      <PaginationLink enabled={page > 1} href={`${href}?page=${page - 1}`}>
         <ChevronLeftIcon className="h-5 w-5" />
+        <span className="sr-only">Previous Page</span>
       </PaginationLink>
       <span>
         page {page} of {pageCount}
       </span>
-      <PaginationLink href={`${href}?page=${page + 1}`}>
+      <PaginationLink
+        enabled={page < pageCount}
+        href={`${href}?page=${page + 1}`}
+      >
         <ChevronRightIcon className="h-5 w-5" />
+        <span className="sr-only">Next Page</span>
       </PaginationLink>
     </div>
   );
 }
 
-function PaginationLink({ children, href }) {
+function PaginationLink({ children, enabled, href }) {
+  if (!enabled) {
+    return (
+      <span className="border cursor-not-allowed rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700">
+        {children}
+      </span>
+    );
+  }
   return (
     <Link
       href={href}
