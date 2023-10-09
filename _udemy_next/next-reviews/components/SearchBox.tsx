fe60c3519f9993eba6f7e10b1a71fa8c489d3616
gangbeomsu <kbs21c1987@gmail.com>
2023-10-09 20:24:00 +0900

    서버상태와 클라이언트 상태의 상태 체크

--- _udemy_next/next-reviews/components/SearchBox.tsx
+++ _udemy_next/next-reviews/components/SearchBox.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { Combobox } from "@headlessui/react";
+import { useIsClient } from "@/lib/hooks";
 
 export default function SearchBox() {
+  const isClient = useIsClient();
+
+  console.log("searchbox isClient", isClient);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <Combobox>
       <Combobox.Input type="text" placeholder="search.." />
