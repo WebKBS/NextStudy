6bb57855eb9361d29afb30aa61b7a9483c70f102
gangbeomsu <kbs21c1987@gmail.com>
2023-10-11 22:17:03 +0900

    메타 태그 추가

--- udemy_next_pageroute/next_blog/pages/index.jsx
+++ udemy_next_pageroute/next_blog/pages/index.jsx
@@ -1,7 +1,12 @@
+import Head from "next/head";
+
 function HomePage() {
   console.log("[index]");
   return (
     <>
+      <Head>
+        <title>My Blog</title>
+      </Head>
       <main>
         <h1>My blog</h1>
       </main>
