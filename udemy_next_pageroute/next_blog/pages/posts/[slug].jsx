e9cb73dd201c6ae1932d997e11ad043fcfdf4384
gangbeomsu <kbs21c1987@gmail.com>
2023-10-14 19:30:08 +0900

    getStaticPaths로 다이나믹 페이지 만들기

--- udemy_next_pageroute/next_blog/pages/posts/[slug].jsx
+++ udemy_next_pageroute/next_blog/pages/posts/[slug].jsx
@@ -1,14 +1,24 @@
 import Head from "next/head";
 import { getPost } from "../../lib/posts";
-import { Html } from "next/document";
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug: "first-post" } },
+      { params: { slug: "second-post" } },
+    ],
+    fallback: false, // fallback false를 설정하면 404페이지가 표시된다.
+  };
+}
 
 // 정적 페이지 생성 getStaticProps
 // getStaticProps는 서버에서 실행 되고 빌드시 클라이언트 사이드이다..
-export async function getStaticProps() {
-  console.log("[첫번째 포스트 페이지] getStaticProps()");
+export async function getStaticProps({ params }) {
+  console.log("[포스트 페이지] getStaticProps()");
+  console.log("params: ", params);
 
   try {
-    const post = await getPost("first-post");
+    const post = await getPost(params.slug);
 
     return {
       props: {
@@ -21,7 +31,7 @@ export async function getStaticProps() {
 }
 
 function FirstPostPage({ post }) {
-  console.log("첫번째 포스트 페이지", { post });
+  console.log("포스트 페이지", { post });
   return (
     <>
       <Head>
