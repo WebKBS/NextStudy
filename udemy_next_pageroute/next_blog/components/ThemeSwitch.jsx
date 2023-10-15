94817f055ad29b88c0441f62f1a447cd1997d903
gangbeomsu <kbs21c1987@gmail.com>
2023-10-15 21:23:06 +0900

    page 라우터 localstorage 사용하기

--- udemy_next_pageroute/next_blog/components/ThemeSwitch.jsx
+++ udemy_next_pageroute/next_blog/components/ThemeSwitch.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import DarkTheme from "./DarkTheme";
 
+function loadDarkMode() {
+  if (typeof localStorage === "undefined") {
+    return false;
+  }
+  const value = localStorage.getItem("darkMode");
+
+  return value === null ? false : JSON.stringify(value);
+}
+
 function ThemeSwitch() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
+
+  const handleClick = () => {
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
 
   const text = darkMode ? "Light Mode" : "Dark Mode";
 
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      {/* 페이지 라우터는 로컬과 서버를 동시에 사용하기때문에 localstorage같은 것은 에러가 날수 있다. */}
+      {/* suppressHydrationWarning 코드를 넣어줘서 경고를 제외하자 */}
+      <button onClick={handleClick} suppressHydrationWarning>
+        {text}
+      </button>
       <style jsx>
         {`
           button {
