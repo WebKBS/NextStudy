78509e3df1b315b385bd44c80b73334d11e5c99c
gangbeomsu <kbs21c1987@gmail.com>
2023-12-07 20:00:39 +0900

    globalthis 추가

--- _udemy_next/next-reviews/lib/db.ts
+++ _udemy_next/next-reviews/lib/db.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient();
+export const db = createPrismaClient();
+
+/** @returns {PrismaClient} */
+function createPrismaClient() {
+  if (!globalThis.prismaClient) {
+    globalThis.prismaClient = new PrismaClient();
+  }
+  return globalThis.prismaClient;
+}
